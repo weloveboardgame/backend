6a6951be21c01f4c14260af9f1f270ee625cc5a3
Xenia <lommy101@gmail.com>
2020-08-22 07:37:34 +0900

    save

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, session, redirect, request, copy_current_request_context, url_for
 from flask_socketio import SocketIO, emit, join_room, rooms, disconnect
-
 async_mode = None
 
 app = Flask(__name__)
@@ -15,9 +14,65 @@ board = [
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
-    [0,0,0,0,0,2,0],
-    [0,0,0,0,1,1,2]
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,1,2]
 ]
+
+import random
+def getRandGravity():
+    tmp = random.randint(0,3)
+    res = ''
+    if tmp == 0: res = 'N'
+    if tmp == 1: res = 'S'
+    if tmp == 2: res = 'W'
+    if tmp == 3: res = 'E'
+
+    return res
+
+def appplyGravity(board, gravityD):
+    if gravityD == 'W':
+        newBoard = [0 for i in range(7)]
+        tempBoard = [0 for i in range(7)]
+
+        for i in range(0,7):
+            tempBoard = [0 for i in range(7)]
+            flag = 0
+            for j in range(0,7):
+                if board[i][j] != 0:
+                    tempBoard[flag] = board[i][j]
+                    flag += 1
+            newBoard[i] = tempBoard
+        return newBoard
+    if gravityD == 'E':
+        newBoard = [0 for i in range(7)]
+        tempBoard = [0 for i in range(7)]
+        for i in range(0,7):
+            tempBoard = [0 for i in range(7)]
+            flag = 0
+            for j in range(0,7):
+                if board[i][j] != 0:
+                    tempBoard[6-flag] = board[i][j]
+                    flag += 1
+            newBoard[i] = tempBoard
+        return newBoard
+    if gravityD == 'N':
+        newBoard = [[0 for col in range(7)] for row in range(7)]
+        for i in range(0,7):
+            flag = 0
+            for j in range(0,7):
+                if board[j][i] != 0:
+                    newBoard[flag][i] = board[j][i]
+                    flag += 1
+        return newBoard
+    if gravityD == 'S':
+        newBoard = [[0 for col in range(7)] for row in range(7)]
+        for i in range(0,7):
+            flag = 0
+            for j in range(0,7):
+                if board[j][i] != 0:
+                    newBoard[6-flag][i] = board[j][i]
+                    flag += 1
+        return newBoard
 
 @app.before_request
 def before_request():
@@ -34,7 +89,7 @@ def close():
 
 @socketio.on('my_event', namespace='/test')
 def test_message(message):
-    emit('my_response', {'data': message['data'], 'board': board})
+    emit('my_response', {'data': message['data'], 'board': board, 'gravity' : 'W'})
 
 @socketio.on('join', namespace='/test')
 def join(message):
@@ -44,7 +99,9 @@ def join(message):
 @socketio.on('my_room_event', namespace='/test')
 def send_room_message(message):
     data = message['data']
-    #emit('my_response', {'data': message['data']}, room=message['room'])
+    data = appplyGravity(data, "N")
+    print(data)
+    emit('my_response', {'data': message['data'], 'board': data}, room=message['room'])
 
 @socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
