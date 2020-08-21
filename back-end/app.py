d92fff165573fd54058cc135fc1807ed24ec84ff
Xenia <lommy101@gmail.com>
2020-08-22 04:43:29 +0900

    save

--- back-end/app.py
+++ back-end/app.py
@@ -1,44 +1,51 @@
-from flask import Flask, render_template, session
-from flask_socketio import SocketIO, emit, join_room, rooms
+from flask import Flask, render_template, session, redirect, request, copy_current_request_context, url_for
+from flask_socketio import SocketIO, emit, join_room, rooms, disconnect
+
+async_mode = None
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode=async_mode)
 
-full = 0
-
-@app.before_request
-def before_request():
-    global full
-    print(full)
-    if full == 2: return "X"
-    elif full == 0: full += 1
-    elif full == 1: full += 1
-    else: full = 1
-    
 @app.route('/')
 def index():
     return render_template('index.htm')
 
-@socketio.on('connect', namespace='/test')
-def connect():
+@app.route('/close')
+def close():
+    return render_template('close.htm')
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': message['data'], 'count': session['receive_count']})
+
+@socketio.on('join', namespace='/test')
+def join(message):
+    join_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': 'In rooms: ' + ', '.join(rooms()), 'count': session['receive_count']})
+
+@socketio.on('my_room_event', namespace='/test')
+def send_room_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': message['data'], 'count': session['receive_count']}, room=message['room'])
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': 'Disconnected!', 'count': session['receive_count']}, callback=can_disconnect)
     
-    print("connected!!")
-    emit('my_response', {'data': 'Connected'})
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my_response', {'data': 'Connected', 'count': 0})
 
-@socketio.on("who", namespace="/test")
-def who(data):
-    if len(str(data).strip()) == 0:
-        return "X"
-    else:
-        print(data["data"])
-
-@socketio.on('disconnect_request', namespace='/mynamespace') 
-def disconnect(): 
-    global full
-    print(full)
-    full -= 1
-    return "disconnected.." 
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
 
 if __name__ == '__main__':
     socketio.run(app)
